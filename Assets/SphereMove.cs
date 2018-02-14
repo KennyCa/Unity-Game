4170db6b758d7937b62a893dfb1363891e49ee37
KennyCa <30661046+KennyCa@users.noreply.github.com>
2018-02-14 11:35:02 -0600

    update

---
+++ Assets/SphereMove.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SphereMove : MonoBehaviour {
+
+    public Animator anim;
+    public float reachDistance;
+    public GameObject player;
+    public GameObject target;
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+        anim = GetComponent<Animator>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        reachDistance = Vector3.Distance(player.transform.position,
+            target.transform.position);
+
+        if ( reachDistance < 70.0f)
+        {
+
+            anim.Play("Sphere");
+            anim.speed = .9f;
+        }
+    }
+}
---
+++ Assets/transport.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class transport : MonoBehaviour {
+
+    public Animator anim;
+    public float reachDistance;
+    public GameObject player;
+    public GameObject target;
+    //public Transform platform;
+    //public Transform newspot;
+    //public float smooth;
+    // Use this for initialization
+    void Start()
+    {
+
+        anim = GetComponent<Animator>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        reachDistance = Vector3.Distance(player.transform.position, target.transform.position);
+
+        if (reachDistance < 5.0f)
+        {
+
+            anim.Play("Platform");
+            anim.speed = .9f;
+            //platform.position = Vector3.Lerp(platform.position, newspot.position, smooth * Time.deltaTime);
+        }
+    }
+}
